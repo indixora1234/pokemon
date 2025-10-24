1fc532267caf102bb8cd1ea26b5025c403e52174
Cate Chew <cate.chew@kcl.ac.uk>
2025-10-24 22:23:25 +0100

    .

--- Assets/Scripts/Pokemons/PokemonBase.cs
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -6,24 +6,24 @@ using System.Collections.Generic;
 
 public class PokemonBase : ScriptableObject
 {
-    [field: SerializeField] public string name {get; private set; };
+    [field: SerializeField] public string name {get; private set; }
 
     [TextArea]
-    [field: SerializeField] public string description {get ; private set; };
+    [field: SerializeField] public string description {get ; private set; }
 
-    [field: SerializeField] public string frontSprite {get ; private set; };
-    [field: SerializeField] public string backSprite {get ; private set; };
+    [field: SerializeField] public string frontSprite {get ; private set; }
+    [field: SerializeField] public string backSprite {get ; private set; }
 
-    [field: SerializeField] public PokemonType type1 {get ; private set; };
-    [field: SerializeField] public PokemonType type2 {get ; private set; };
+    [field: SerializeField] public PokemonType type1 {get ; private set; }
+    [field: SerializeField] public PokemonType type2 {get ; private set; }
 
     //Base Stats
-    [field: SerializeField] public int maxHp {get ; private set; };
-    [field: SerializeField] public int attack {get ; private set; };
-    [field: SerializeField] public int defense {get ; private set; };
-    [field: SerializeField] public int spAttack {get ; private set; };
-    [field: SerializeField] public int spDefense {get ; private set; };
-    [field: SerializeField] public int speed {get ; private set; };
+    [field: SerializeField] public int maxHp {get ; private set; }
+    [field: SerializeField] public int attack {get ; private set; }
+    [field: SerializeField] public int defense {get ; private set; }
+    [field: SerializeField] public int spAttack {get ; private set; }
+    [field: SerializeField] public int spDefense {get ; private set; }
+    [field: SerializeField] public int speed {get ; private set; }
 
 
 }
