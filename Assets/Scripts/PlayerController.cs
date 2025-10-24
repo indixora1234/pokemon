a54df21863c5c3da6902fc5a1b14a719c432ea72
Cate Chew <cate.chew@kcl.ac.uk>
2025-10-24 21:32:54 +0100

    added PokemonBase script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private LayerMask solidObjectsLayer;
+
+    public bool isMoving;
+    private Vector2 input;
+
+    private void Update(){
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //remove diagonal movement
+            if (input.x != 0 ) input.y = 0;
+
+            if (input != Vector2.zero){
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (IsWalkable(targetPos)){
+                    StartCoroutine(Move(targetPos));
+                } 
+            }
+        }
+    }
+
+    //Coroutine for smooth, tile-by-tile movement
+    IEnumerator Move(Vector3 targetPos) {
+
+        isMoving = true;
+        //if there's a difference between the current and target position
+        //player will move towards target position
+        //loop will only stop once they're equal
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon){
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null; 
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    //Handles collisions 
+    //(Player doesn't move forward when it's colliding with a solid object)
+    private bool IsWalkable(Vector3 targetPos){
+        if ((Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer)) != null){
+            return false;
+        }
+        return true;
+    }
+}
--- Assets/Scripts/Pokemons/PokemonBase.cs
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -1,6 +1,50 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class PokemonBase
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")]
+
+public class PokemonBase : ScriptableObject
 {
-    
+    [field: SerializeField] public string name {get; private set; };
+
+    [TextArea]
+    [field: SerializeField] public string description {get ; private set; };
+
+    [field: SerializeField] public string frontSprite {get ; private set; };
+    [field: SerializeField] public string backSprite {get ; private set; };
+
+    [field: SerializeField] public PokemonType type1 {get ; private set; };
+    [field: SerializeField] public PokemonType type2 {get ; private set; };
+
+    //Base Stats
+    [field: SerializeField] public int maxHp {get ; private set; };
+    [field: SerializeField] public int attack {get ; private set; };
+    [field: SerializeField] public int defense {get ; private set; };
+    [field: SerializeField] public int spAttack {get ; private set; };
+    [field: SerializeField] public int spDefense {get ; private set; };
+    [field: SerializeField] public int speed {get ; private set; };
+
+
+}
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Griffin,
 }
